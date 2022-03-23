932de61eb1d45dc378c70febb3faec998722bca8
inhwanK <inhan1009@naver.com>
2022-03-23 18:00:13 +0900

    커밋 테스트

--- src/main/java/org/example/springtest/SpringtestApplication.java
+++ src/main/java/org/example/springtest/SpringtestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringtestApplication {
-
+//	주석 추가
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtestApplication.class, args);
 	}
